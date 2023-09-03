2d74ed4d29c36778abf817e211ba01afc75a0ccd
Magdalena Djordjevic <magda@ipb.ac.rs>
2023-09-03 10:44:48 +0200

    novi Vasa programi

--- Use_enumerate.py
+++ Use_enumerate.py
@@ -1,5 +1,11 @@
-#napisi program koji za listu datu ispod stampa elemnte te liste zajedno sa rednim brojevima u listi. Redni brojevi treba da budu takvi da je prvi element u listi 1 (ne nula). Dakle izlaz programa treba da bude:
-#1 <vrednost prvog elementa u listi>
-#2 <vrednost drugog elementa u listi>
+#napisi program koji za listu datu ispod stampa elemnte te liste ispisuje zajedno sa rednim brojevima u listi. Redni brojevi treba da budu takvi da je prvi element u listi 1 (ne nula). Dakle izlaz programa treba da bude:
+#1. <vrednost prvog elementa u listi>
+#2. <vrednost drugog elementa u listi>
 #....
 #Za resenje zadatka koristi enumerate funkciju.
+
+lista = [ "1" , "5" , "8" , "17" ]
+
+for broj,elem in enumerate(lista):
+
+    print(f"{broj+1}. {elem}")
