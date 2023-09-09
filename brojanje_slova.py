f08dd7f4ea9a023d4509313ba4ed30374fd079e2
MarkoDjor <dmarko@bio.bg.ac.rs>
2023-09-09 21:03:23 +0200

    velika slova i brojanje slova

---
+++ brojanje_slova.py
@@ -0,0 +1,1 @@
+#na ulazu korisnik treba da unese tekst. Treba da se napise program koji ce u tom tekstu da izbroje broj slova i da taj broj odstampa na ekranu. Prazna mesta u tekstu se ne racunaju. Npr. ako se unese "Vasa je odlican programer", program treba da odstampa "Broj slova u tekstu je 22".
---
+++ velikaSlova.py
@@ -0,0 +1,1 @@
+#na ulazu korisnik treba da unese tekst. Taj tekst onda treba da se prebaci u velika slova i odstampa na ekranu. Npr. ako se unese "Vasa je odlican programer", treba da se odstampa: "VASA JE ODLICAN PROGRAMER".
